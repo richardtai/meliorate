e45a981a6d4b5fa6a8f1a5a89697d657b5a6e374
Richard Tai <richardtai@rescomp-12-207010.stanford.edu>
2013-03-21 20:11:50 -0700

    meliorate-server push.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize'),
-    sequelize = new Sequelize ('meliorate_db', 'richardtai', null, {
+    sequelize = new Sequelize ('meliorate_db', 'postgres', 'postgres', {
       dialect: 'postgres',
       // When connecting to the database, psql uses 'local socket' as the default
       // method of connecting, while sequelize uses 'host'. Therefore, specify this 
